5290b0cb53fdb09b8b2214254756a3a2b6f3cc6e
Smartoneinok <Alexander_Novotny@aol.com>
2021-11-01 11:14:01 -0500

    Updates for the hello world example

--- box-custom-skills-starter-gcp/README.md
+++ box-custom-skills-starter-gcp/README.md
@@ -1,15 +1,19 @@
 # Sample hello-world Skill on GCP
 
-This is a sample hello-world nodejs skill designed to work on Goole Cloud Platform.
+This is a sample hello-world nodejs Box Skill designed to work on Goole Cloud Platform.
 
 **Note: You will need to add a gcp crendentials file and update the serverless.yml with that appropriate connection information.**
 
 ## Steps to Setup and Deploy
 
 1. Install Node v10.0.0 or higher
-2. [Set up Google Cloud Account](https://serverless.com/framework/docs/providers/google/guide/credentials/)
+2. [Set up a Google Cloud Account](https://serverless.com/framework/docs/providers/google/guide/credentials/)
 3. Download the code.
 4. Add your google keyfile to the .gcloud folder and name is serverless.json.
 5. Update any connection and configuration information in the serverless.yml and package.json files. 
 6. `npm install`
 7. `sls deploy`
+8. Once the deploy is complete, copy the invocation URL and paste it into the Box Skill configuration section.
+
+**In order to allow Box to call the serverless function, you need to add permissions for allUsers to the serverless function for the Cloud Functions Invoker role.** 
+
--- box-custom-skills-starter-gcp/index.js
+++ box-custom-skills-starter-gcp/index.js
@@ -1,18 +1,38 @@
 'use strict';
 
 const { FilesReader, SkillsWriter } = require('./skills-kit-2.0');
+const Box = require("box-node-sdk");
 
-exports.boxSkill = async (request, response) => {
-    const filesReader = new FilesReader(request.body);
-    const skillsWriter = new SkillsWriter(filesReader.getFileContext());
+//environment variables
+const boxPrimaryKey = process.env.box_primary_key;
+const boxSecondaryKey = process.env.box_secondary_key;
 
-    await skillsWriter.fileWriteClient.files.addMetadata(
-        skillsWriter.fileId,
-        skillsWriter.fileWriteClient.metadata.scopes.GLOBAL,
-        'properties',
-        { skillMessage: 'Hello World!' }
-    );
+exports.boxSkill = async (request, response) => {    
+    const body = JSON.stringify(request.body);
+    console.log('Request Body: ', body);
+    
+    //Validate Bpx Signature Keys So Bad People Don't Use Your Endpoint
+    let isValid = Box.validateWebhookMessage(request.body, request.headers, boxPrimaryKey, boxSecondaryKey);
 
-    response.status(200).send('Hello World!');
+    if(isValid){
+        const filesReader = new FilesReader(request.body);
+        const skillsWriter = new SkillsWriter(filesReader.getFileContext());
+        
+        try{
+            //Example custom metadata template added to file
+            await skillsWriter.fileWriteClient.files.addMetadata(
+                skillsWriter.fileId,
+                skillsWriter.fileWriteClient.metadata.scopes.GLOBAL,
+                'properties',
+                { skillMessage: 'Hello World!' }
+            );
+            response.status(200).send('Hello World');
+        } catch (error) {
+            console.log("Error:", JSON.stringify(error))
+            response.status(200).send(error);
+        }
+    } else {
+        console.log('Keys Were Not Valid')
+        response.status(200).send('Keys Invalid');
+    }
 };
-
