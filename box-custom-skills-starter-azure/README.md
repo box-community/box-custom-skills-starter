0fd773900ad8600f43daffe9b7523b98788c5116
Smartoneinok <Alexander_Novotny@aol.com>
2022-11-30 15:25:29 -0600

    adding azure base

---
+++ box-custom-skills-starter-azure/README.md
@@ -0,0 +1,10 @@
+# Sample hello-world Skill on IBM
+
+This is a sample hello-world nodejs Box Skill designed to work on azure.
+
+
+## Steps to Setup and Deploy
+
+1. Install Node v10.0.0 or higher
+
+TBD
---
+++ box-custom-skills-starter-azure/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Import FilesReader and SkillsWriter classes from skills-kit-2.0.js library
+const { FilesReader, SkillsWriter, SkillsErrorEnum } = require('./skills-kit-2.0');
+const Box = require('box-node-sdk');
+
+//environment variables
+//const boxPrimaryKey = process.env.box_primary_key;
+//const boxSecondaryKey = process.env.box_secondary_key;
+
+//TBD
